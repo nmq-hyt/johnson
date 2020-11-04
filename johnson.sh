101625f26b68e9bfa2e2d538e7752454990c4174
nmq-hyt <conoroboyle83@gmail.com>
2020-11-04 01:15:46 +0000

    A better version, without cp because I don't need to copy the files to a seperate DIR

--- johnson.sh
+++ johnson.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-cp -ri --target-directory=/home/nmq-hyt/Backups /home/nmq-hyt/Projects /home/nmq-hyt/Documents
 cd /home/nmq-hyt/Backups
-tar -cf nmq-hyt_backup_$(date +%d%b%y).tar Projects  Documents 
-cd 
+tar -cf nmq-hyt_backup_$(date +%d%b%y).tar /home/nmq-hyt/Projects /home/nmq-hyt/Documents  
+mv  nmq-hyt_backup_$(date +%d%b%y).tar GitPort
+cd GitPort
+git add nmq-hyt_backup_$(date +%d%b%y).tar 
+git commit -m "Another one"
+git push origin master
+# obviously git asks me for my github password here, so I'm not going to just write it out and upload it to The Internet
+cd
