a998b9a6f5cc9a6e9a3a5333e49d9a409f205f9a
nmq-hyt <conoroboyle83@gmail.com>
2021-03-22 03:36:34 +0000

    added check for backup directory, added external media backup

--- johnson_local.sh
+++ johnson_local.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 #
 # johnson cron daily
+# replace /home/nmq-hyt/ with your /home/name directory
 FILE=/home/nmq-hyt/Backups/backup_$(date +%d%b%y).tar
-#FILEDATE=$(date +%a)
-if test -e "$FILE"
+DIRTEST=/home/nmq-hyt/Backups
+MEDIADIR=/media/nmq-hyt/"XUBUNTU 18_"/Backup
+if test -d "$DIRTEST"
 then
-	echo "Backup already exists"
-else 
-	rw * ~/Backups
+	echo "Backup directory exists"
+	if test -f "$FILE"
+	then
+		echo "Backup exists"
+	else
+		rm  ~/Backups/*
+		tar -cf backup_$(date +%d%b%y).tar /home/nmq-hyt/Projects /home/nmq-hyt/Documents
+		mv backup_$(date +%d%b%y).tar ~/Backups
+		cp ~/Backups/backup_$(date +%d%b%y).tar  "$MEDIADIR";
+	fi
+else
+	mkdir ~/Backups
 	tar -cf backup_$(date +%d%b%y).tar /home/nmq-hyt/Projects /home/nmq-hyt/Documents
 	mv backup_$(date +%d%b%y).tar ~/Backups
-
 fi
