1cc12165ba22f6aca4dc925fc57e2e8b4b7bcec1
nmq-hyt <conoroboyle83@gmail.com>
2021-03-22 02:37:21 +0000

    fixed redundancy prevention logic; changed so that backups every day

--- johnson_local.sh
+++ johnson_local.sh
@@ -2,15 +2,13 @@
 #
 # johnson cron daily
 FILE=/home/nmq-hyt/Backups/backup_$(date +%d%b%y).tar
-FILEDATE=$(date +%a)
+#FILEDATE=$(date +%a)
 if test -e "$FILE"
 then
-echo "Backup already exists"
-elif test "$FILEDATE" = "Sun"
-then
-rw * ~/Backups
-else
-tar -cf backup_$(date +%d%b%y).tar /home/nmq-hyt/Projects /home/nmq-hyt/Documents
+	echo "Backup already exists"
+else 
+	rw * ~/Backups
+	tar -cf backup_$(date +%d%b%y).tar /home/nmq-hyt/Projects /home/nmq-hyt/Documents
+	mv backup_$(date +%d%b%y).tar ~/Backups
 
-mv backup_$(date +%d%b%y).tar ~/Backups
 fi
