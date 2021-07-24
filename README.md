08980b70b8f84e82f861c56bbf68566ce845d4aa
nmq-hyt <conoroboyle83@gmail.com>
2021-07-24 22:10:06 +0100

    simplfied; added check for flash drive and copy to flash drive

--- README.md
+++
@@ -1,4 +0,0 @@
-A small bash script to backup my files, written after my nth OS crash.
-
-It tars the entirety of my most personally important folders, and moves it to my own Backups folder, from which I manually sync to my external USB drive.
-This is running on cron on my home system.
--- johnson_local.sh
+++ johnson_local.sh
@@ -2,23 +2,27 @@
 #
 # johnson cron daily
 # replace /home/nmq-hyt/ with your /home/name directory
+# to do
+# script input accepts
+# various directory names
 FILE=/home/nmq-hyt/Backups/backup_$(date +%d%b%y).tar
-DIRTEST=/home/nmq-hyt/Backups
-MEDIADIR=/media/nmq-hyt/"XUBUNTU 18_"/Backup
-if test -d "$DIRTEST"
-then
-	echo "Backup directory exists"
-	if test -f "$FILE"
-	then
+MEDIADIR=/media/nmq-hyt/"XUBUNTU 18_"
+
+if test -f "$FILE"; then
 		echo "Backup exists"
-	else
+    # this is my flash drive mount, and it'll always be a
+		if test -d "$MEDIADIR";then
+        rm /media/nmq-hyt/"XUBUNTU 18_"/Backup/*
+        cp ~/Backups/* /media/nmq-hyt/"XUBUNTU 18_"/Backup
+        umount /dev/sdb1
+        fi
+else
 		rm  ~/Backups/*
 		tar -cf backup_$(date +%d%b%y).tar /home/nmq-hyt/Projects /home/nmq-hyt/Documents
 		mv backup_$(date +%d%b%y).tar ~/Backups
-		cp ~/Backups/backup_$(date +%d%b%y).tar  "$MEDIADIR";
-	fi
-else
-	mkdir ~/Backups
-	tar -cf backup_$(date +%d%b%y).tar /home/nmq-hyt/Projects /home/nmq-hyt/Documents
-	mv backup_$(date +%d%b%y).tar ~/Backups
-fi
+    if test -d "$MEDIADIR";then
+        rm /media/nmq-hyt/"XUBUNTU 18_"/Backup/*
+        cp ~/Backups/* /media/nmq-hyt/"XUBUNTU 18_"/Backup
+        umount /dev/sdb1
+        fi
+	  fi
